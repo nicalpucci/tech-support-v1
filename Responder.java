cccbad17f3bd1ab5f8cc654ab364590a61fb2ec0
nicolas <nicalpucci@gmail.com>
2021-02-25 11:40:07 +0100

    HashMap implementado

--- Responder.java
+++ Responder.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.HashMap;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -11,6 +12,7 @@ public class Responder
 {
     private Random azar = new Random();
     private ArrayList <String> respuestas = new ArrayList <String>();
+    private HashMap <String, String> hashInput;
     /**
      * Construct a Responder - nothing to do
      */
@@ -18,6 +20,7 @@ public class Responder
     {
         respuestas = new ArrayList<String>();
         azar = new Random();
+        hashInput = new HashMap <String, String>();
         respuestas.add("I can solve this problem.");
         respuestas.add("Do u want to learn English?");
         respuestas.add("Mira aprende inglés macho porque no te entiendo nada.");
@@ -26,14 +29,25 @@ public class Responder
         respuestas.add("Ahhh filho da puta agora sim entendo");
         respuestas.add("Are you a believer?");
         respuestas.add("Nice joke");
+        hashInput.put("deficient", "Oh don´t worry I can solve it");
+        hashInput.put("español", "Vale, mejor hablamos en español");
+        hashInput.put("pandemia", "Eso a mi no me repercute");
+        hashInput.put("KFC", "llama al 900 711 117");
     }
 
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
-    public String generateResponse()
+    public String generateResponse(String userInput)
     {
-        return respuestas.get(azar.nextInt(respuestas.size()));
+        String resultado;
+        if(hashInput.get(userInput) == null){
+            resultado = respuestas.get(azar.nextInt(respuestas.size()));
+        } 
+        else {
+            resultado = hashInput.get(userInput); 
+        }
+        return resultado;   
     }
 }
--- SupportSystem.java
+++ SupportSystem.java
@@ -42,7 +42,7 @@ public class SupportSystem
                 finished = true;
             }
             else {
-                String response = responder.generateResponse();
+                String response = responder.generateResponse(input);
                 System.out.println(response);
             }
         }
