8d937268a031df6353ce620915aacb97aa84516d
nicolas <nicalpucci@gmail.com>
2021-03-04 12:42:52 +0100

    método generateResponder admite y devuelve conjuntos de HashSet

--- Responder.java
+++ Responder.java
@@ -13,7 +13,10 @@ public class Responder
 {
     private Random azar = new Random();
     private ArrayList <String> respuestas = new ArrayList <String>();
-    private HashMap <String, String> hashInput;
+    private HashMap <HashSet<String>, String> hashInput;
+    private HashSet <String> conjunto = new HashSet <String>();
+    private HashSet <String> conjunto2 = new HashSet <String>();
+    private HashSet <String> conjunto3 = new HashSet <String>();
     /**
      * Construct a Responder - nothing to do
      */
@@ -21,7 +24,8 @@ public class Responder
     {
         respuestas = new ArrayList<String>();
         azar = new Random();
-        hashInput = new HashMap <String, String>();
+        hashInput = new HashMap <HashSet<String>, String>();
+        conjunto = new HashSet <>();
         respuestas.add("I can solve this problem.");
         respuestas.add("Do u want to learn English?");
         respuestas.add("Mira aprende inglés macho porque no te entiendo nada.");
@@ -30,10 +34,17 @@ public class Responder
         respuestas.add("Ahhh filho da puta agora sim entendo");
         respuestas.add("Are you a believer?");
         respuestas.add("Nice joke");
-        hashInput.put("deficient", "Oh don´t worry I can solve it");
-        hashInput.put("español", "Vale, mejor hablamos en español");
-        hashInput.put("pandemia", "Eso a mi no me repercute");
-        hashInput.put("kfc", "llama al 900 711 117");
+        conjunto.add("free");
+        conjunto.add("app");
+        conjunto2.add("kfc");
+        conjunto2.add("español");
+        conjunto3.add("crash");
+        conjunto3.add("linux");
+        conjunto3.add("problem");
+        hashInput.put(conjunto, "You can download our app in Play Store -not App Store- for free!");
+        hashInput.put(conjunto2, "Vale, mejor hablamos en español");
+        hashInput.put(conjunto3, "Perhaps... did you drop your computer?");
+        hashInput.put(conjunto2, "llama al 900 711 117");        
     }
 
     /**
@@ -42,25 +53,27 @@ public class Responder
      */
     public String generateResponse(HashSet<String> userInput)
     {
-        String respuestaFinal = null;
-        boolean respuestaAleatoria = true;
-        for(String elemento : userInput){
-            if(hashInput.containsKey(elemento)){
-                respuestaFinal = hashInput.get(elemento);
-                respuestaAleatoria = false;
-            }
-        }
-        if(respuestaAleatoria){
+        String respuestaFinal = hashInput.get(userInput);
+        if(respuestaFinal == null){
             respuestaFinal = respuestas.get(azar.nextInt(respuestas.size()));
         }    
         return respuestaFinal;
-        /*
-        if(hashInput.get(userInput) == null){
-        resultado = respuestas.get(azar.nextInt(respuestas.size()));
-        } 
-        else {
-        resultado = hashInput.get(userInput); 
+
+        /*for(HashSet<String> claveActual : hashInput.keySet()){
+        for(String palabraUsuarioActual : userInput){
+        if(palabraUsuarioActual.equals(claveActual)){
+        respuestaFinal = hashInput.get(palabraUsuarioActual);
+        respuestaAleatoria = false;
         }
-         */
+        }
+        if(hashInput.containsKey(claveActual)){
+        respuestaFinal = hashInput.get(claveActual);
+        respuestaAleatoria = false;
+        }
+        }
+        if(respuestaAleatoria){
+        respuestaFinal = respuestas.get(azar.nextInt(respuestas.size()));
+        }    
+        return respuestaFinal;*/
     }
 }
